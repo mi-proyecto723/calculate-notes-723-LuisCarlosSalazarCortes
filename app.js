4de4dc65f94a330a30d52f456918290a9272b3b2
luiscar2050 <luis3206162549@gmail.com>
2024-10-18 09:25:01 -0500

    calculadora casi terminada

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-const username = document.getElementById("name");
-const note1 = document.getElementById("nota1");
-const note2 = document.getElementById("nota2");
-const note3 = document.getElementById("nota3");
-const calculateButton = document.getElementById("btn-calculate");
-const response = document.getElementById("resultado");
+const userName = document.getElementById('nameUser');
+const data1 = document.getElementById('note1');
+const data2 = document.getElementById('note2');
+const data3 = document.getElementById('note3');
+const calculateButton = document.getElementById('btn-calculate');
+const btnPredict = document.getElementById('btn-predict');
+const response = document.getElementById('response');
 
-calculateButton.addEventListener("click", calculateNote);
+btnPredict.addEventListener('click', predictNote);
+calculateButton.addEventListener('click', calculateNote); 
 
 function calculateNote(event) {
-    let user = username.value; 
-    let grade1 = Number(note1.value);
-    let grade2 = Number(note2.value); 
-    let grade3 = Number(note3.value);
-    
-    let result = (grade1 * 0.3) + (grade2 * 0.3) + (grade3 * 0.4);
-    
-    response.textContent = "Sr. " + user + ", su nota es de: " + result.toFixed(2); 
+    event.preventDefault(); 
+
+    let user = userName.value;
+    let note1 = Number(data1.value);
+    let note2 = Number(data2.value);
+    let note3 = Number(data3.value);
+
+    let result = (note1 * 0.3) + (note2 * 0.3) + (note3 * 0.4);
+
+    if ((note1 >= 1 && note1 <= 5) && (note2 >= 1 && note2 <= 5) && (note3 >= 1 && note3 <= 5)) {
+        if (result < 3.5) {
+            response.style.color = 'black';
+            response.textContent = `Sr ${user} perdió :( su nota es de: ${result}`; 
+        } else if (result >= 3.5 && result <= 4.5) {
+            response.style.color = 'orange';
+            response.textContent = `Sr ${user} pasó, su nota es de: ${result}`; 
+        } else {
+            response.style.color = 'green';
+            response.textContent = `Sr ${user}, excelente, su nota es de: ${result}`; 
+        }
+    } else {
+        response.textContent = 'Ingrese notas válidas'; 
+    }
+}
+
+function predictNote(event) {
     event.preventDefault();
+
+    let user = userName.value;
+    let note1 = Number(data1.value);
+    let note2 = Number(data2.value);
+    let notaMin = 3.5;
+
+    let operation = (notaMin - (note1 * 0.3) - (note2 * 0.3)) / 0.4;
+    data3.value = operation;
+    alert(`Sr ${user}, la nota mínima debe ser de ${operation}`);
 }
